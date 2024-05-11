79be14ed3a41801505c9341d2631cb9bae69c9dd
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-11 13:25:35 -0500

    feat(component textBox)

---
+++ src/app/shared/components/text-box/text-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-text-box',
+  templateUrl: './text-box.component.html',
+  styleUrl: './text-box.component.css'
+})
+export class TextBoxComponent {
+
+}

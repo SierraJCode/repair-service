0a6751c48534af1dfb5a797a40c90bee42bf23d6
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-13 17:10:32 -0500

    fix(module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { RepairCenterModule } from './repair-center/repair-center.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    RepairCenterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { TextBoxComponent } from './components/text-box/text-box.component';
 
@@ -21,7 +23,8 @@ import { TextBoxComponent } from './components/text-box/text-box.component';
   ],
   exports: [
     NavBarComponent,
-    TextBoxComponent
+    TextBoxComponent,
+    AboutPageComponent,
   ]
 })
 export class SharedModule { }

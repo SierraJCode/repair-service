df6993bf117b55e18e0108fab7a13d41bf1995b8
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-13 17:09:55 -0500

    feat(repairCenter)

---
+++ src/app/repair-center/repair-center.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServicePageComponent } from './pages/service-page/service-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ServicePageComponent,
+    AboutPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ServicePageComponent,
+  ]
+})
+export class RepairCenterModule { }

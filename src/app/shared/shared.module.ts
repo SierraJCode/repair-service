d61b4922b496727be99f2f0f5c0fb0f54d562624
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-16 12:45:30 -0500

    feat(assets|sharedModule)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { TextBoxComponent } from './components/text-box/text-box.component';
 
@@ -12,9 +8,9 @@ import { TextBoxComponent } from './components/text-box/text-box.component';
 
 @NgModule({
   declarations: [
-    HomePageComponent,
-    AboutPageComponent,
-    ContactPageComponent,
+    // HomePageComponent,
+    // AboutPageComponent,
+    // ContactPageComponent,
     NavBarComponent,
     TextBoxComponent
   ],
@@ -24,7 +20,7 @@ import { TextBoxComponent } from './components/text-box/text-box.component';
   exports: [
     NavBarComponent,
     TextBoxComponent,
-    AboutPageComponent,
+    // AboutPageComponent,
   ]
 })
 export class SharedModule { }

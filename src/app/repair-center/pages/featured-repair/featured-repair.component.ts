d55335caa67989ec98cf488da7eda0a5ed17bcf1
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-16 12:44:52 -0500

    feat(add repairCenter section)

---
+++ src/app/repair-center/pages/featured-repair/featured-repair.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'repair-featured',
+  templateUrl: './featured-repair.component.html',
+  styleUrl: './featured-repair.component.css'
+})
+export class FeaturedRepairComponent {
+
+}
--- src/app/repair-center/repair-center.module.ts
+++ src/app/repair-center/repair-center.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicePageComponent } from './pages/service-page/service-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { FeaturedRepairComponent } from './pages/featured-repair/featured-repair.component';
 
 
 
 @NgModule({
   declarations: [
     ServicePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    ContactPageComponent,
+    FeaturedRepairComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ServicePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    FeaturedRepairComponent
   ]
 })
 export class RepairCenterModule { }

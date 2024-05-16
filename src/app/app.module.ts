e13212effce9988b20f0c1ea32afc8972037ad04
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-16 14:29:53 -0500

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
-
 import { AppComponent } from './app.component';
+
 import { SharedModule } from './shared/shared.module';
 import { RepairCenterModule } from './repair-center/repair-center.module';
 
@@ -13,6 +13,7 @@ import { RepairCenterModule } from './repair-center/repair-center.module';
   ],
   imports: [
     BrowserModule,
+    // FontAwesomeModule,
     AppRoutingModule,
     SharedModule,
     RepairCenterModule

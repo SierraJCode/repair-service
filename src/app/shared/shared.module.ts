4a46c1eff4fa566cd7446f74fd0bbba187c902ff
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-16 14:28:14 -0500

    update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,30 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { TextBoxComponent } from './components/text-box/text-box.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent,
-    AboutPageComponent,
-    ContactPageComponent,
     NavBarComponent,
-    TextBoxComponent
+    TextBoxComponent,
   ],
   imports: [
     CommonModule,
+    FontAwesomeModule
+    
   ],
   exports: [
     NavBarComponent,
     TextBoxComponent,
-    AboutPageComponent,
   ]
 })
 export class SharedModule { }

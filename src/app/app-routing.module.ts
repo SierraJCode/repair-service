b0e796f4db6275a4e27205300e66520815fd211b
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-18 13:16:01 -0500

    add lazyPath

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'repair-center', loadChildren: () => import('./repair-center/repair-center.module').then(m => m.RepairCenterModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

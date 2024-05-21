b26bb6980ff1120e73bb076c44d5ef285fbd1a49
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-20 22:04:54 -0500

    feat(facilities pages)

---
+++ src/app/repair-center/pages/facilities/facilities.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'repair-facilities',
+  templateUrl: './facilities.component.html',
+  styleUrl: './facilities.component.css'
+})
+export class FacilitiesComponent {
+
+}

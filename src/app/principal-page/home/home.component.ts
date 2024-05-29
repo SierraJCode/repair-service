22abb2c2d3af4b8dd393af9b304a28c929585510
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-28 22:50:10 -0500

    refactoring component

--- src/app/principal-page/home/home.component.ts
+++ src/app/principal-page/home/home.component.ts
@@ -18,4 +18,14 @@ export class HomeComponent {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+  facilities = [
+    { img: 'assets/caja.jpg', title: 'Reparación Centralita Bosch Motronic M1.1 / M1.2 / M1.3' },
+    { img: 'assets/caja.jpg', title: 'Reparación Centralita ABS TRW EBC 450 ABS Ford' },
+    { img: 'assets/caja.jpg', title: 'Reparación Centralita Bosch EDC15C6 Mercedes CR2.Xx' },
+    { img: 'assets/caja.jpg', title: 'Reparación Centralita Bosch Motronic M1.1 / M1.2 / M1.3' },
+    { img: 'assets/caja.jpg', title: 'Reparación Centralita Bosch EDC15C6 Mercedes CR2.Xx' },
+    { img: 'assets/caja.jpg', title: 'Reparación Centralita ABS TRW EBC 450 ABS Ford' },
+    { img: 'assets/caja.jpg', title: 'Reparación Centralita ABS TRW EBC 450 ABS Ford' }
+  ];
+
 }

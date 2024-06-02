65f62efaf704c4a2b5e55ac360d2d35d5aee78ba
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-02 17:21:59 -0500

    remove repair-center

--- src/app/repair-center/repair-center.module.ts
+++ src/app/repair-center/repair-center.module.ts
@@ -2,25 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import { ServicePageComponent } from './pages/service-page/service-page.component';
+
 import { FacilitiesComponent } from './pages/facilities/facilities.component';
-import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 
 
 
 @NgModule({
   declarations: [
-    ServicePageComponent,
     FacilitiesComponent,
-    TestimonialsComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ServicePageComponent,
     FacilitiesComponent,
-    TestimonialsComponent,
   ]
 })
 

2db3787dddc5cddb3eabf17e0d673000b012b399
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-07 22:19:51 -0500

    changes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 
 import { PrincipalPageModule } from './principal-page/principal-page.module';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -13,7 +16,12 @@ import { PrincipalPageModule } from './principal-page/principal-page.module';
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
+    AppRoutingModule,
+    CommonModule,
+
+    FontAwesomeModule,
+
+    SharedModule,
     PrincipalPageModule,
   ],
   providers: [],

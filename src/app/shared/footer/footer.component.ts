ca939eae667662ab147bc54bf9425c5d7089dc9d
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-07 22:21:08 -0500

    shared component

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { icon } from './../../icons';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  icon = icon
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [
+    FooterComponent
+  ]
+})
+export class SharedModule { }

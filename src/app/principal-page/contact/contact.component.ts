debb3f7338709582f1010422deafbadc19e51d00
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-10 16:20:11 -0500

    feat(contact page structure)

---
+++ src/app/principal-page/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { icon } from './../../icons';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  icon = icon;
+
+}

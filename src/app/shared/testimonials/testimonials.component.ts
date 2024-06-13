ea3cf66b792c55008ed1405e12780f46291bb7d4
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-12 22:16:31 -0500

    structure test

---
+++ src/app/shared/testimonials/testimonials.component.ts
@@ -0,0 +1,12 @@
+import { icon } from './../../icons';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-testimonials',
+  templateUrl: './testimonials.component.html',
+  styleUrl: './testimonials.component.css'
+})
+export class TestimonialsComponent {
+  icon = icon;
+
+}

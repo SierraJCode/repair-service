bc55cc38a914c22463667e33ca7978eb4a00dcc6
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-18 21:41:58 -0500

    feat: changes desing for uptimization

--- src/app/principal-page/about/about.component.ts
+++ src/app/principal-page/about/about.component.ts
@@ -9,4 +9,13 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   icon=icon;
 
+  images = [
+    { src: 'assets/Ion/ion-1.jpg' },
+    { src: 'assets/Ion/ion-2.jpg' },
+    { src: 'assets/Ion/ion-3.jpg' },
+    { src: 'assets/Ion/ion-4.jpg' },
+    { src: 'assets/Ion/ion-5.jpg' },
+    { src: 'assets/Ion/ion-6.jpg' },
+  ];
+
 }

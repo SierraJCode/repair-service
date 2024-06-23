137d83abcb6c9523656701784e05f690257a1505
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-23 08:43:14 -0500

    feat: changes in routing module, redirect to the page not-found and changes in styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './principal-page/home/home.component';
 import { AboutComponent } from './principal-page/about/about.component';
 import { ContactComponent } from './principal-page/contact/contact.component';
+import { NotFoundComponent } from './principal-page/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
-  {path: '**', component: HomeComponent}
+  {path: '**', component: NotFoundComponent}
 
 
 
--- src/app/principal-page/not-found/not-found.component.ts
+++ src/app/principal-page/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  constructor(private router: Router) {}
+
+  goBack(): void {
+    this.router.navigate(['/']); // Navega a la página principal
+  }
 
 }

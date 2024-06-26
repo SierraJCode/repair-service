d9d85a71b582d1a3af9cfe185db090a82326fc53
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-25 22:48:16 -0500

    feat: add interface for service.model, data management | add data modularization and easy update or changes

---
+++ src/app/data/data.ts
@@ -0,0 +1,87 @@
+import { Service } from "../interface/service.model";
+
+export const SERVICES: Service[] = [
+  {
+    name: 'Delphi Delco Multec HSFI-2.x / HDRC',
+    image: 'assets/caja.jpg',
+    description: 'Centralitas (ECU\'s)',
+    problems: [
+      'Sin activación de la bobina',
+      'Sin activación de los inyectores',
+      'Mensaje de error en el circuito de 5V'
+    ],
+    marca: 'Opel',
+    modelo: 'Modelo1',
+    serie: 'Serie1',
+    oem: 'OEM1234'
+  },
+  {
+    name: 'VDO MSM A-Class (LHM)',
+    image: 'assets/caja.jpg',
+    description: 'Centralitas (ECU\'s)',
+    problems: [
+      'Sin activación de la bobina',
+      'Sin activación de los inyectores',
+      'Sin activación del motor de mando del ralentí',
+      'Mensaje de error en el circuito de 5V',
+      'Diversas averías',
+      'P0230 - No hay control de rele de bomba de combustible.'
+    ],
+    marca: 'Mercedes',
+    modelo: 'Modelo1',
+    serie: 'Serie1',
+    oem: 'OEM1234'
+  },
+  {
+    name: 'Bosch PSG 5 (EDU from VP30 / VP44)',
+    image: 'assets/caja.jpg',
+    description: 'Centralitas (ECU\'s)',
+    problems: [
+      'Sin activación de la bobina',
+      'Sin activación de los inyectores',
+      'Sin activación del motor de mando del ralentí',
+      'Mensaje de error en el circuito de 5V',
+      'Diversas averías',
+      'P0230 - No hay control de rele de bomba de combustible.'
+    ],
+    marca: 'Mercedes',
+    modelo: 'Modelo1',
+    serie: 'Serie1',
+    oem: 'OEM1234'
+  },
+  {
+    name: 'VDO MSM A-Class (LHM)',
+    image: 'assets/caja.jpg',
+    description: 'Centralitas (ECU\'s)',
+    problems: [
+      'Sin activación de la bobina',
+      'Sin activación de los inyectores',
+      'Sin activación del motor de mando del ralentí',
+      'Mensaje de error en el circuito de 5V',
+      'Diversas averías',
+      'P0230 - No hay control de rele de bomba de combustible.'
+    ],
+    marca: 'BMW',
+    modelo: 'Modelo1',
+    serie: 'Serie1',
+    oem: 'OEM1234'
+  },
+  {
+    name: 'VDO MSM A-Class (LHM)',
+    image: 'assets/caja.jpg',
+    description: 'Centralitas (ECU\'s)',
+    problems: [
+      'Sin activación de la bobina',
+      'Sin activación de los inyectores',
+      'Sin activación del motor de mando del ralentí',
+      'Mensaje de error en el circuito de 5V',
+      'Diversas averías',
+      'P0230 - No hay control de rele de bomba de combustible.'
+    ],
+    marca: 'BMW',
+    modelo: 'Modelo1',
+    serie: 'Serie1',
+    oem: 'OEM1234'
+  },
+  
+];
---
+++ src/app/interface/service.model.ts
@@ -0,0 +1,10 @@
+export interface Service {
+  name: string;
+  image: string;
+  description: string;
+  problems: string[];
+  marca: string;
+  modelo: string;
+  serie: string;
+  oem: string;
+}

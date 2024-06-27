e945eceff358b5b2fa6fec7091935267aacc751f
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-06-27 12:05:08 -0500

    feat: remove repair.services, update to service.service structure

--- src/app/providers/service.service.ts
+++ src/app/providers/service.service.ts
@@ -8,12 +8,21 @@ import { SERVICES } from '../data/data';
 })
 export class ServiceService {
   private services: Service[] = SERVICES;
+  private servicesH : Service[] =[]
 
   constructor() { }
 
   getServices(): Service[] {
     return this.services;
-  }
+  };
+
+  getServicesHome(): Service[] {
+    const indexSelect = [2,5,6,7,8,1,2,4,5,8,9,10]
+    this.servicesH = indexSelect.map(index => this.services[index])
+    return this.servicesH
+  };
+
+
 
   getServiceByMarca(marca: string): Service[] {
     return this.services.filter(service => service.marca === marca);
